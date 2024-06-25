d473a19a29f7a00eee3b9f83924c593e5e78c418
Prathm8 <159471585+Prathm8@users.noreply.github.com>
2024-06-25 21:59:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-provider "aws" {
-  region = "us-west-1"
-}
 
 resource "aws_instance" "dev" {
+     region = "us-west-1"
     ami = "ami-0124ee9682f33ad99"
     instance_type = "t2.nano"
     tags = {
