4b1f8affe390ae6b824f67b7aff7fd4c40a35522
Prathm8 <159471585+Prathm8@users.noreply.github.com>
2024-06-26 13:19:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "aws_instance" "dev" {
     instance_type = "t2.nano"
     tags = {
     Name = "dev-ec2"
-    access_key = "AKIA6ODU4PBCXGMAAX34"
-    secret_key = "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"
+    
     }
 }
